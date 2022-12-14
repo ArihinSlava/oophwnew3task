17a29233f82d9f5d42adc6cf6a1842c8dcf49dc4
Arihin <arihin.slava@yandex.ru>
2022-12-14 10:47:55 +0300

    дз2 задание 2

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,15 @@ public class Car {
     int year;
     String country;
 
+    Car(String brand, String model, double engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
     void information() {
         System.out.println(brand + " " + model + ". " + year + " год выпуска , сборка " + country +  ". " + color + " цвет кузова , объем двигателя - " + engineVolume + "л.");
     }
